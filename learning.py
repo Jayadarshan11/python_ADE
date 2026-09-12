446ee2a3b7e1944a6a2252449007c25c87e316e4
Jayadarshan11 <jayadarshan20@gmail.com>
2026-09-12 17:56:30 +0530

    added notebook

--- learning.py
+++ learning.py
@@ -1,2 +1,28 @@
 Author = 'Jayadarshan'
 print(" Welcome to python programming : By", Author)
+
+'''
+Beginners :- installation guide 
+
+1) To learn the python programming first please go to :-https://www.w3schools.com/python/default.asp
+and learn some info or basics to understand 
+
+2) now we can easily start the programms what we learned at w3schools site 
+
+---> steps to practice hands on
+---> 1) install the python latest version from their official website called :- https://www.python.org/
+---> in intsall phase please intall at c:drive  why because c drive is mainly use for the Appliactions only 
+---> for practicing we can create the workspace at the d : drive 
+---> 2) After installation check at cmd (command prompt)   command :- python --version it will show the latest version 
+---> 3) After for easy practicing we are using one tool called vscode install this from :- https://code.visualstudio.com/download
+---> 4) After that we need to install the extension called python from vscode (this is the bridge between vscode and python)
+---> 5) create one folder at d drive name it python and add one file name it as learning.py 
+---> 6) name the file .py so that system understands it is python file 
+---> write simple progrem --> print('hello world') save it and run the terminal
+---> you get the output and also check the terminal with the command python --version
+ 
+----  Now you successfully complete the setup, now you can start your python journey
+
+-----------------------------  Thank you --------------------------------------
+
+'''
